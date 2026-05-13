183439409cb30bf4c6849cd19e90041e8a2acb7f
Maciej Mazur <drwisz@gmail.com>
2026-05-13 11:00:57 +0200

    cleanup: gitignore, usun pycache i patch scripts

--- patch_hss_demo.py
+++
@@ -1,7 +0,0 @@
-with open('hss_demo.py', 'r') as f:
-    content = f.read()
-
-content = content.replace("N = 64          # wymiar wektora", "N = 15          # wymiar wektora")
-
-with open('hss_demo.py', 'w') as f:
-    f.write(content)
